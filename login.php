5fba4ec0be7fe5e283c6a443f82dc6693b251556
Mohit Kishore <kishoremohit07@gmail.com>
2019-02-14 23:28:47 +0530

    Update login.php

--- login.php
+++ login.php
@@ -19,7 +19,7 @@
   </div>
   <div class="input-group">
     <label>Password</label>
-    <input type="password" name="Password_1">
+    <input type="password" name="password_1">
   </div>
   <div class="input-group">
     <button type="submit" name="login" class="button">Login</button>
