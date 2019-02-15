a722c7e1f0e582b82bae635805b8d0b6fda3b500
gurpreet97 <44726136+gurpreet97@users.noreply.github.com>
2019-02-15 02:44:08 -0800

    Update login.php

--- login.php
+++ login.php
@@ -15,22 +15,23 @@
     mysqli_set_charset($dbc, "utf8");
 
     
-		$query1 = "SELECT password FROM guest_users WHERE username = $username";
-		$query2 = "SELECT username FROM guest_users WHERE password = $password";
+		$query1 = "SELECT password FROM guest_users WHERE username = '$username'";
+		$query2 = "SELECT username FROM guest_users WHERE password = '$password'";
 	    $result1 = mysqli_query($dbc,$query1);
 	    $result2 =  mysqli_query($dbc,$query2);
 	    $row1 = mysqli_fetch_array($result1,MYSQLI_ASSOC);
 	    $row2 = mysqli_fetch_array($result2,MYSQLI_ASSOC);
 	    $count1 = mysqli_num_rows($result1);
 	    $count2 = mysqli_num_rows($result2);
-	    if(count1==1 && $result1==$password){
-	    	//login successful
+        
+	    if($count1==1 && $row1["password"]==$password){
+	    	echo "login successful";
 	    }
 	    else{
-	    	if(count1==0){
+	    	if($count1==0){
 	    		echo "username don't exist"."<br>";
 	    	}
-	    	else if(count1==1){
+	    	else if($count1==1){
 	    		echo "incorrect password"."<br>";	
 	    	}
 	    }
