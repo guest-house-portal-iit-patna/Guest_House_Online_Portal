1fbc6a00b9e302f183ca8f582a7a48621cd09e4d
gurpreet97 <44726136+gurpreet97@users.noreply.github.com>
2019-02-15 02:45:27 -0800

    Update loginform.php

--- loginform.php
+++ loginform.php
@@ -19,7 +19,7 @@
   </div>
   <div class="input-group">
     <label>Password</label>
-    <input type="password" name="password_1">
+    <input type="password" name="password">
   </div>
   <div class="input-group">
     <button type="submit" name="login" class="button">Login</button>
