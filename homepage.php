fed8401153c6a6e8cdebb4df87f675404893299a
Mohit Kishore <kishoremohit07@gmail.com>
2019-02-21 11:47:17 +0530

    Update homepage.php

--- homepage.php
+++ homepage.php
@@ -10,7 +10,7 @@ if(empty($_SESSION['username'])) {
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Home</title>
-  <link rel="stylesheet" type="text/css" title="Cool stylesheet" href="register.css">
+  <link rel="stylesheet" type="text/css" title="Cool stylesheet" href="css/loginsystem.css">
 </head>
 <body>
   <div id="root">
