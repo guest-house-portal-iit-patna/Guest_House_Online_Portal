adca13035b4f381dede2040049bd6e3d699eaa2b
Mohit Kishore <kishoremohit07@gmail.com>
2019-02-21 11:38:48 +0530

    Update login.php

--- login.php
+++ login.php
@@ -1,25 +1,30 @@
+<?php include('server.php'); ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Register</title>
-  <link rel="stylesheet" type="text/css" title="Cool stylesheet" href="register.css">
+  <link rel="stylesheet" type="text/css" title="Cool stylesheet" href="css/loginsystem.css">
 </head>
 <body>
   <div id="root"></div>
-  <script src="main.js" type="text/javascript"></script>
+  <!-- <script src="main.js" type="text/javascript"></script> -->
 <div class="header">
   <h2>Login</h2>
 </div>
 <form class="login" action="login.php" method="post">
+
+  <!-- The erros are displayed here-->
+  <?php include('errors.php'); ?>
+
   <div class="input-group">
     <label>Username</label>
-    <input type="text" name="username">
+    <input type="text" name="username" required>
   </div>
   <div class="input-group">
     <label>Password</label>
-    <input type="password" name="password_1">
+    <input type="password" name="password" required>
   </div>
   <div class="input-group">
     <button type="submit" name="login" class="button">Login</button>
