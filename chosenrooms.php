37e89c5dd5178254dee5dba32ff3e873be8412d0
gurpreet97 <44726136+gurpreet97@users.noreply.github.com>
2019-04-08 20:41:32 +0530

    added mail in chosenrooms.php

--- chosenrooms.php
+++ chosenrooms.php
@@ -1,7 +1,12 @@
 <?php
+
+// Import PHPMailer classes into the global namespace
+// These must be at the top of your script, not inside a function
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
 require_once('server.php');
 $id=$_SESSION['id'];
-
+$email=$_SESSION['email'];
 if (isset($_POST['roomschosen'])){
   if(!empty($_POST['check_list'])){
   // Loop to store and display values of individual checked checkbox.
@@ -23,5 +28,45 @@ if (isset($_POST['roomschosen'])){
 }
 header("location: homepage.php");
 }
+
+  // Load Composer's autoloader
+  require 'phpmailer/vendor/autoload.php';
+
+  // Instantiation and passing `true` enables exceptions
+  $mail = new PHPMailer(true);
+
+      //Server settings
+      $mail->SMTPDebug = 1;                                       // Enable verbose debug output
+      $mail->isSMTP();                                            // Set mailer to use SMTP
+      $mail->Host       = 'smtp.gmail.com';  // Specify main and backup SMTP servers
+      $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
+      $mail->Username   = 'theoriginalmk7@gmail.com';                     // SMTP username
+      $mail->Password   = 'uqmftsMfU9ustcw';                               // SMTP password
+      $mail->SMTPSecure = 'tls';                                  // Enable TLS encryption, `ssl` also accepted
+      $mail->Port       = 587;                                    // TCP port to connect to
+
+      //Recipients
+      $mail->setFrom('theoriginalmk7@gmail.com', 'Guesthouse IIT PATNA');
+      $mail->addAddress($email);     // Add a recipient
+      //$mail->addAddress('ellen@example.com');               // Name is optional
+      // $mail->addReplyTo('info@example.com', 'Information');
+      // $mail->addCC('cc@example.com');
+      // $mail->addBCC('bcc@example.com');
+
+      // // Attachments
+      // $mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
+      // $mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
+
+      // Content
+      $mail->isHTML(true);                                  // Set email format to HTML
+      $mail->Subject = 'Booking request received.';
+      $mail->Body    = "Hello ".$_SESSION['username'].",<br><br>Your request for booking has been registered.<br>Your booking ID is ".$_SESSION['id'].". <br>You will be suitably notified the status of your booking. <br>
+      <br> Thank you";
+      //$mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+
+      if($mail->send())
+          echo 'Message has been sent';
+      else
+          echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
 
 ?>
