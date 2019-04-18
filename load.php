d17dc631515f17e465eb617ebfce1191a9bce0e9
gurpreet97 <44726136+gurpreet97@users.noreply.github.com>
2019-04-19 01:39:54 +0530

    added calendar view

---
+++ load.php
@@ -0,0 +1,28 @@
+<?php
+
+//load.php
+
+$connect = new PDO('mysql:host=localhost;dbname=guesthouse', 'root', '');
+$data = array();
+
+$query = "SELECT * FROM info ORDER BY id";
+
+$statement = $connect->prepare($query);
+
+$statement->execute();
+
+$result = $statement->fetchAll();
+
+foreach($result as $row)
+{
+ $data[] = array(
+  'id'   => $row["id"],
+  'title'   => $row["title"],
+  'start'   => $row["start_event"],
+  'end'   => $row["end_event"]
+ );
+}
+
+echo json_encode($data);
+
+?>
---
+++ loading.php
@@ -0,0 +1,68 @@
+<?php
+
+//load.php
+
+$connect = new PDO('mysql:host=localhost;dbname=guesthouse', 'root', '');
+$date1 = strtotime(date("Y-m-d 00:00:00"));
+//$today = date("Y-m-d H:i:s");
+
+$dbcc= mysqli_connect('localhost','root','','guesthouse');
+$query = "DELETE FROM info";
+$data1 = mysqli_query($dbcc, $query);
+$date = date("Y-m-d", $date1);
+for($i = 0 ; $i <= 10; $i++){
+    $from_date1 = $date1;
+    $from_date = date("Y-m-d 00:00:00", $from_date1);
+    $to_date1 = strtotime("+1 day", $from_date1);
+    $to_date = date("Y-m-d 00:00:00", $to_date1);
+    print("from ".$from_date."= ".gettype($from_date)."<br>");
+    print("to ".$to_date."= ".gettype($from_date)."<br>");
+    $normal = 0;
+    $master = 0;
+    $dbcc= mysqli_connect('localhost','root','','guesthouse');
+    if (!$dbcc) {
+      die("Connection failed: " . mysqli_connect_error());
+    }
+    $query = "SELECT room FROM bookedrooms WHERE arrival<='$from_date' AND departure>='$from_date' OR arrival<='$to_date' AND departure>='$to_date' OR arrival>='$from_date' AND departure<='$to_date'";
+    $data1 = mysqli_query($dbcc, $query);
+    while($r=mysqli_fetch_assoc($data1)){
+      $roomsarr[]=$r['room'];
+    }
+
+    $query = "SELECT room,type,floor FROM rooms";
+    $data = mysqli_query($dbcc, $query);
+
+  if(mysqli_num_rows($data) != 0){
+
+      $row2=array();
+      while($row = mysqli_fetch_array($data))
+      {
+        $row2[]=$row;
+      }
+       foreach ($row2 as $row1)
+          {
+              if(!isset($roomsarr))
+              {
+                  $normal = 16;
+                  $master = 8;
+              }
+              else
+              {
+                if(!(in_array($row1['room'],$roomsarr)))
+                {
+                  if($row1['type']=="Normal")$normal++;
+                  else $master++;
+                }
+              }
+
+
+          }
+          }
+          $str = "Normal: ".$normal."\nMaster: ".$master;
+          $query = "INSERT INTO info (title, start_event, end_event) VALUES ('$str','$from_date','$from_date')";
+          mysqli_query($dbcc,$query);
+          $date1 = strtotime("+1 day", $date1);
+          unset($roomsarr);
+}
+header('location: calendarview.php');
+?>
